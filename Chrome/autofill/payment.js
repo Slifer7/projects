03d697c2554df99c17632c1dff71dbdc35f81dd4
tdquang7@gmail.com <tdquang7@gmail.com>
2016-06-11 09:09:01 +0700

    Update month and year of cc

--- Chrome/autofill/payment.js
+++ Chrome/autofill/payment.js
@@ -7,6 +7,11 @@ chrome.storage.sync.get({
   $("#checkout_credit_card_number").val(item.cardnumber);
   $("#checkout_credit_card_name").val(item.nameoncard);
   $("#checkout_credit_card_expiry").val(item.date);
+
+  var parts = item.date.split("/");
+
+  $("#checkout_credit_card_month").val(parts[0]);
+  $("#checkout_credit_card_year").val(parts[1]);
   $("#checkout_credit_card_verification_value").val(item.cvv);
 
   $(".step__footer__continue-btn")[0].click();
