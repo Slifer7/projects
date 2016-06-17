0d765167cd1accb7fb79d9178a4613f1a7440369
tdquang7@gmail.com <tdquang7@gmail.com>
2016-06-17 20:44:45 +0700

    Bo sung hinh model follio de bid upwork

---
+++ CSharp/SystemTrayApp/ContextMenus.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SystemTrayApp.Properties;
+using System.Drawing;
+
+namespace SystemTrayApp
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	class ContextMenus
+	{
+
+		/// <summary>
+		/// Creates this instance.
+		/// </summary>
+		/// <returns>ContextMenuStrip</returns>
+		public ContextMenuStrip Create()
+		{
+			// Add the default menu options.
+			ContextMenuStrip menu = new ContextMenuStrip();
+			ToolStripMenuItem item;
+			ToolStripSeparator sep;
+
+			// Windows Explorer.
+			item = new ToolStripMenuItem();
+			item.Text = "google.com";
+			item.Click += new EventHandler(Explorer_Click);
+			item.Image = Resources.freelancer;
+			menu.Items.Add(item);
+
+			// About.
+			item = new ToolStripMenuItem();
+			item.Text = "freelancer.com";
+			item.Click += new EventHandler(About_Click);
+			item.Image = Resources.google;
+			menu.Items.Add(item);
+
+			// Separator.
+			sep = new ToolStripSeparator();
+			menu.Items.Add(sep);
+
+			// Exit.
+			item = new ToolStripMenuItem();
+			item.Text = "Exit";
+			item.Click += new System.EventHandler(Exit_Click);
+			item.Image = Resources.Exit;
+			menu.Items.Add(item);
+
+			return menu;
+		}
+
+		/// <summary>
+		/// Handles the Click event of the Explorer control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		void Explorer_Click(object sender, EventArgs e)
+		{
+            System.Diagnostics.Process.Start("http://google.com");
+
+        }
+
+		/// <summary>
+		/// Handles the Click event of the About control.
+		/// </summary>
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		void About_Click(object sender, EventArgs e)
+		{
+            System.Diagnostics.Process.Start("http://google.com");
+        }
+
+		/// <summary>
+		/// Processes a menu item.
+		/// </summary>
+		/// <param name="sender">The sender.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		void Exit_Click(object sender, EventArgs e)
+		{
+			// Quit without further ado.
+			Application.Exit();
+		}
+	}
+}
